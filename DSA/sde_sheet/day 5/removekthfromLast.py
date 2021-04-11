94b9c6b26b8a5950b737372dfbea350be168093f
kaydee0502 <kshitijdhama513@gmail.com>
2021-04-11 13:39:38 +0530

    remove kth node from last

---
+++ DSA/sde_sheet/day 5/removekthfromLast.py
@@ -0,0 +1,65 @@
+'''
+
+19. Remove Nth Node From End of List
+Medium
+
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Follow up: Could you do this in one pass?
+
+ 
+
+Example 1:
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+ 
+
+Constraints:
+
+    The number of nodes in the list is sz.
+    1 <= sz <= 30
+    0 <= Node.val <= 100
+    1 <= n <= sz
+
+
+
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        A = ListNode(0)
+        A.next = head
+        
+        f = A
+        s = A
+        
+        
+        for i in range(n):
+            f = f.next
+            
+        while f.next:
+            f = f.next
+            s = s.next
+            
+        s.next = s.next.next
+        
+        return A.next
+        s
