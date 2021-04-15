d5cecb5a87974daba3de0c5612f1b9772fe9b30f
kaydee0502 <kshitijdhama513@gmail.com>
2021-04-15 16:42:47 +0530

    unique subsets

---
+++ DSA/sde_sheet/day 9/uniquesubset.py
@@ -0,0 +1,45 @@
+'''
+
+90. Subsets II
+Medium
+
+Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2]
+Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 10
+    -10 <= nums[i] <= 10
+
+
+
+'''
+
+
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        ret = []
+        self.dfs(sorted(nums), [], ret)
+        return ret
+    
+    def dfs(self, nums, path, ret):
+        ret.append(path)
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            self.dfs(nums[i+1:], path+[nums[i]], ret)
