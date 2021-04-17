332959eee65e7e3527cac2c11379e81b911c1e70
kaydee0502 <kshitijdhama513@gmail.com>
2021-04-17 08:50:19 +0530

    reverse linked list recursive

---
+++ DSA/Recursion/reversellist_recursive.py
@@ -0,0 +1,61 @@
+'''
+206. Reverse Linked List
+Easy
+
+6769
+
+129
+
+Add to List
+
+Share
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+ 
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    
+    def util(self,node,head):
+        if not head:
+            return node
+        
+        temp = head.next
+        head.next = node
+        
+        return self.util(head,temp)
+        
+    def reverseList(self, head: ListNode) -> ListNode:
+        
+        return self.util(None,head)
+        
