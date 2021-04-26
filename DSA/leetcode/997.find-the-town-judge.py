de68101cefb4ec794147abe07c00e129167d4412
kaydee0502 <kshitijdhama513@gmail.com>
2021-04-26 15:26:55 +0530

    minor changer

--- DSA/leetcode/997.find-the-town-judge.py
+++ DSA/leetcode/997.find-the-town-judge.py
@@ -11,14 +11,15 @@ class Solution:
     
     def findJudge(self, N: int, trust: List[List[int]]) -> int:
         
-        nei = [0] * (N+1)
+        ine = [0] * (N+1)
+        out = [0] * (N+1)
         
         for i in trust:
-            nei[i[0]] -=1
-            nei[i[1]] +=1
+            ine[i[1]] += 1
+            out[i[0]] += 1
             
         for i in range(1,N+1):
-            if nei[i] == N-1:
+            if ine[i] == N-1 and out[i] == 0:
                 return i
             
         return -1
