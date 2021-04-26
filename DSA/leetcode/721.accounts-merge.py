2560c16b1151e24011005606b15d91ebe648aaaa
kaydee0502 <kshitijdhama513@gmail.com>
2021-04-26 15:26:16 +0530

    account merge

--- DSA/leetcode/721.accounts-merge.py
+++ DSA/leetcode/721.accounts-merge.py
@@ -13,5 +13,42 @@ class Solution:
     
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         
+        
+        uniSet = {}
+        rev = {}
+        
+        def find(x):
+            if not x in uniSet:
+                uniSet[x] = x
+                
+            if uniSet[x] != x:
+                return find(uniSet[x])
+            return uniSet[x]
+        
+        
+        def union(x,y):
+            uniSet[find(x)] = find(y)
+            
+        for n,*em in accounts:
+            
+            if len(em) == 1:
+                find(em[0])
+            #print(n)
+            for i,j in itertools.combinations(em,2):
+                union(i,j)
+                
+            for i in em:
+                if not i in rev:
+                    rev[i] = n
+                    
+            
+        grp = collections.defaultdict(list)
+        
+        for e in uniSet:
+            grp[find(e)].append(e)
+            
+        #print(rev)
+        return [[rev[parent]] + sorted(childs) for parent,childs in grp.items()]
+            
 # @lc code=end
 
