db5f30933a379507fb62c208b88ad5885b1392b3
kaydee0502 <kshitijdhama513@gmail.com>
2021-04-26 11:29:06 +0530

    medain of two sorted arrarys

---
+++ DSA/leetcode/721.accounts-merge.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=721 lang=python3
+#
+# [721] Accounts Merge
+#
+
+# @lc code=start
+from collections import defaultdict
+class Solution:
+    
+    def __init__(self):
+        self.g = defaultdict(list)
+    
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        
+# @lc code=end
+
