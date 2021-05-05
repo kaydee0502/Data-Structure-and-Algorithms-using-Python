1111502e00b5765a90d8d50185fd5e4b88a0d5c7
kaydee0502 <kshitijdhama513@gmail.com>
2021-05-05 21:21:27 +0530

    not adj matrix

--- DSA/cp/a.py
+++ DSA/cp/a.py
@@ -1,12 +1,35 @@
-def makebold(f):
-    return lambda: "<b>" + f() + "</b>"
-def makeitalic(f):
-    return lambda: "<i>" + f() + "</i>"
+t = int(input())
 
-@makebold
-@makeitalic
-def say():
-    return "Hello"
-
-print(say())
+while t:
     
+    
+    n = int(input())
+    if n == 2:
+        print(-1)
+        t-=1
+        continue
+        
+    mat = [[0 for i in range(n)] for j in range(n)]
+    
+    c = 1
+    for i in range(n):
+        for j in range(n):
+            
+            if (i+j)%2 == 0:
+              
+                mat[i][j] = c
+                c+=1
+                
+    
+    for i in range(n):
+        for j in range(n):
+            
+            if mat[i][j] == 0:
+                
+                mat[i][j] = c
+                c+=1
+            
+    for i in mat:
+        print(*i)
+    
+    t-=1
