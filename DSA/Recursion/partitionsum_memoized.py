073ab22d202d00936c606baef499fd5cdce8844d
kaydee0502 <kshitijdhama513@gmail.com>
2021-05-06 11:20:25 +0530

    partition sum memoized

---
+++ DSA/Recursion/partitionsum_memoized.py
@@ -0,0 +1,28 @@
+class Solution:
+   
+    def __init__(self):
+        self.cache = {}
+    
+    def part(self,t,i,csum,nums):
+        
+        if i < 0 and csum == t:
+            return True
+        
+        if i < 0:
+            return False
+        
+        if (i,csum) in self.cache:
+            return self.cache[(i,csum)]
+        
+        
+        p = self.part(t,i-1,csum+nums[i],nums)
+        n = self.part(t,i-1,csum-nums[i],nums)
+        
+        self.cache[(i,csum)] = p+n
+        return p+n
+    
+    def canPartition(self, nums: List[int]) -> bool:
+        target = 0
+        
+        return self.part(target,len(nums)-1,0,nums)
+        
