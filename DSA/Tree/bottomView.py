c1dbec1664489d89140ac1bbeb702930cc53a570
kaydee0502 <kshitijdhama513@gmail.com>
2021-05-30 13:58:35 +0530

    bottom view of a binary tree

---
+++ DSA/Tree/bottomView.py
@@ -0,0 +1,200 @@
+'''
+
+Bottom View of Binary Tree 
+Medium Accuracy: 45.32% Submissions: 69253 Points: 4
+Given a binary tree, print the bottom view from left to right.
+A node is included in bottom view if it can be seen when we look at the tree from bottom.
+
+                      20
+                    /    \
+                  8       22
+                /   \        \
+              5      3       25
+                    /   \      
+                  10    14
+
+For the above tree, the bottom view is 5 10 3 14 25.
+If there are multiple bottom-most nodes for a horizontal distance from root, then print the later one in level traversal. For example, in the below diagram, 3 and 4 are both the bottommost nodes at horizontal distance 0, we need to print 4.
+
+                      20
+                    /    \
+                  8       22
+                /   \     /   \
+              5      3 4     25
+                     /    \      
+                 10       14
+
+For the above tree the output should be 5 10 4 14 25.
+ 
+
+Example 1:
+
+Input:
+       1
+     /   \
+    3     2
+Output: 3 1 2
+Explanation:
+First case represents a tree with 3 nodes
+and 2 edges where root is 1, left child of
+1 is 3 and right child of 1 is 2.
+
+Thus nodes of the binary tree will be
+printed as such 3 1 2.
+Example 2:
+
+Input:
+         10
+       /    \
+      20    30
+     /  \
+    40   60
+Output: 40 20 60 30
+Your Task:
+This is a functional problem, you don't need to care about input, just complete the function bottomView() which takes the root node of the tree as input and returns an array containing the bottom view of the given tree.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(N).
+
+
+'''
+
+#User function Template for python3
+
+
+#Function to return a list containing the bottom view of the given tree.
+
+from collections import deque
+def bottomView(root):
+    
+    ha = {}
+    
+    
+    def iot(root,l):
+        nonlocal ha
+        
+        
+        q = deque()
+        q.append([root,0])
+        
+        
+        while q:
+            r,l = q.popleft()
+            
+            ha[l] = r.data
+            
+            if r.left:
+                q.append([r.left,l-1])
+                
+            if r.right:
+                q.append([r.right,l+1])
+    
+        
+        
+        
+    if not root:
+        return
+    iot(root,0)
+    res = []
+    for i in sorted(list(ha.keys())):
+        res.append(ha[i])
+    
+    #print(ha)
+    return res
+    
+    # code here
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import sys
+sys.setrecursionlimit(50000)
+#Contributed by Sudarshan Sharma
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        res = bottomView(root)
+        for i in res:
+            print (i, end = " ")
+        print()
+
+
+# } Driver Code Ends
